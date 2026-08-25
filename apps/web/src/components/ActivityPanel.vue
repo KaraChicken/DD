e6e452633c821e718aaa43a8e0884761bea433a6
DORO廚 <86883208+KaraChicken@users.noreply.github.com>
2026-08-25 22:14:56 +0800

    refactor(web): extract activity panel

---
+++ apps/web/src/components/ActivityPanel.vue
@@ -0,0 +1,8 @@
+<template>
+  <a class="first" href="/Web/recarga/" target="_blank" title="">
+    <img alt="今日活動" src="/Web/Assets/images/shop.jpg" />
+  </a>
+  <a href="#" target="_blank" title="">
+    <img alt="十一月專題活動" src="/Web/Assets/images/s0.jpg" />
+  </a>
+</template>
