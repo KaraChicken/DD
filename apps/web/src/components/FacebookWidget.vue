5d57aedc6ebefbc8393df46829f8f5ecdbf031fd
DORO廚 <86883208+KaraChicken@users.noreply.github.com>
2026-08-25 22:14:42 +0800

    refactor(web): extract reusable Facebook widget

---
+++ apps/web/src/components/FacebookWidget.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2 class="h2-tit">Facebook</h2>
+  <div class="widget">
+    <iframe
+      allow="encrypted-media"
+      allowtransparency="true"
+      frameborder="0"
+      height="500"
+      scrolling="no"
+      src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FDDTDEV%2F&amp;tabs=timeline&amp;width=259&amp;height=500&amp;small_header=false&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=true&amp;appId=172608803671801"
+      style="border:none;overflow:hidden"
+      width="340"
+    ></iframe>
+  </div>
+</template>
