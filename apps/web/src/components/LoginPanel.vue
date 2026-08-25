0e3a6143d6f7f7d549fa0b25ff29662fe3dc64a2
DORO廚 <86883208+KaraChicken@users.noreply.github.com>
2026-08-25 22:14:30 +0800

    refactor(web): extract reusable login panel

---
+++ apps/web/src/components/LoginPanel.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const emit = defineEmits<{
+  login: [username: string, password: string]
+  register: []
+  recover: []
+}>()
+
+const username = ref('')
+const password = ref('')
+
+function submit() {
+  emit('login', username.value, password.value)
+}
+</script>
+
+<template>
+  <div class="unlogin-box clearfix">
+    <form method="POST" action="" @submit.prevent="submit">
+      <div class="input-box f-l p-r">
+        <input v-model="username" type="text" name="UserName" placeholder="Usuario" autocomplete="username" />
+        <input v-model="password" type="password" name="Password" placeholder="Senha" autocomplete="current-password" />
+        <div class="blank"></div>
+      </div>
+      <button class="loginbtn f-l" type="submit" name="login"></button>
+    </form>
+    <div class="forget f-l">
+      <a href="#" @click.prevent="emit('register')">Registrar conta</a> |
+      <a href="#" @click.prevent="emit('recover')">Recuperar senha?</a>
+    </div>
+  </div>
+</template>
